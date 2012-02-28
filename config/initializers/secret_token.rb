2125e866ecd67768e4104f0546665dca5ea07f06
aesacus <jayrbolton@gmail.com>
2012-02-28 14:15:09 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskgarden::Application.config.secret_token = 'b3169cb9df8130f70a38be4dcec556325840fd72b663233c79c0b0fe2d7f29bd7955f10b7a4989391dd2c5cde1bff52a544cfaa15ad54a33b82e7e605a8a6a14'
